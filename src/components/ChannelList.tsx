f889f9e03955a41f651b27a9bce51a71d081e39a
ka2yuki <mori1103@gmail.com>
2023-05-26 11:50:51 +0900

    Add: new component.

---
+++ src/components/ChannelList.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Menu } from 'semantic-ui-react'
+
+interface Props {
+    
+}
+ 
+interface State {
+    
+}
+ 
+class ChannelList extends React.Component<Props, State> {
+    state = { hoge: 0 }
+    render() { 
+        return ( 
+         );
+    }
+}
+ 
+export default ChannelList;
